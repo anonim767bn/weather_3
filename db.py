cb65a96d4d6debfc11c7a584545cddb1521ddb79
siriusdevs <tenigin.aa@talantiuspeh.ru>
2024-02-15 12:14:24 +0300

    добавлена страница с погодой

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ import os
 import dotenv
 import psycopg
 
-from config import CITIES_DELETE, CITIES_INSERT, CITIES_SELECT
+from config import CITIES_DELETE, CITIES_INSERT, CITIES_SELECT, CITY_SINGLE_SELECT
 
 DEFAULT_PORT = 5555
 
@@ -26,6 +26,11 @@ def connect() -> tuple[psycopg.Connection, psycopg.Cursor]:
 def get_cities(cursor: psycopg.Cursor) -> list[tuple]:
     cursor.execute(CITIES_SELECT)
     return cursor.fetchall()
+
+
+def get_city(cursor: psycopg.Cursor, city_name: str) -> list[tuple]:
+    cursor.execute(CITY_SINGLE_SELECT, params=(city_name,))
+    return cursor.fetchone()
 
 
 def add_city(connection: psycopg.Connection, cursor: psycopg.Cursor, city_coord: tuple) -> bool:
--- server.py
+++ server.py
@@ -4,35 +4,64 @@ from typing import Optional
 from urllib.parse import unquote
 
 import db
-
-HOST, PORT = '127.0.0.1', 8000
-OK = 200
-BAD_REQUEST = 400
-SERVER_ERROR = 500
-CREATED = 201
-NO_CONTENT = 204
-NOT_FOUND = 404
-HEADER_TYPE = 'Content-Type'
-HEADER_LEN = 'Content-Length'
-POST_KEYS = 'name', 'lat', 'lon'
+from config import *
+from views import page_from_cities, main_page
+from weather import get_weather
 
 
 def json_from_cities(cities: list[tuple]) -> str:
     return json.dumps({name: {'lat': lat, 'lon': lon} for name, lat, lon in cities})
 
 
+def cities_to_strings(cities: list[tuple]) -> list:
+    return [f'{city}\t{lat}\t{lon}' for city, lat, lon in cities]
+
+
 class CustomHandler(SimpleHTTPRequestHandler):
     db_connection, cursor = db.connect()
 
-    def respond(self, code: int, message: Optional[str] = None) -> None:
-        self.send_response(code, message)
-        self.send_header(HEADER_TYPE, 'text/json')
-        self.end_headers()
+    def router(self):
+        if self.path.startswith('/cities'):
+            return self.cities()
+        elif self.path.startswith('/weather'):
+            return self.weather()
+        else:
+            return main_page()
+
+    def get_query(self) -> dict:
+        quest_mark = '?'
+        if quest_mark not in self.path:
+            return {}
+        query = self.path[self.path.index(quest_mark)+1:]
+        return {pair.split('=')[0]: pair.split('=')[1] for pair in query.split('&')}
+
+    def weather(self):
+        CITY_KEY = 'city'
+        query = self.get_query()
+        if CITY_KEY in query.keys():
+            response = db.get_city(self.cursor, query[CITY_KEY])
+            if response:
+                return str(get_weather(*response))
+        return ''
+
+    def cities(self):
+        cities = db.get_cities(self.cursor)
+        if GET_RETURNS == 'json':
+            body = json_from_cities(cities)
+        elif GET_RETURNS == 'html':
+            body = page_from_cities(cities_to_strings(cities))
+        else:
+            body = ''
+        return body
 
     def do_GET(self) -> None:
-        cities = db.get_cities(self.cursor)
         self.respond(OK)
-        self.wfile.write(json_from_cities(cities).encode())
+        self.wfile.write(self.router().encode())
+
+    def respond(self, code: int, message: Optional[str] = None) -> None:
+        self.send_response(code, message)
+        self.send_header(HEADER_TYPE, f'text/{GET_RETURNS}')
+        self.end_headers()
 
     def do_POST(self) -> None:
         body_len = self.headers.get(HEADER_LEN)
